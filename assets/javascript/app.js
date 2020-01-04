f776d362652d912cef2cd9de95c6508387072cc2
stylianos kouroupas <steliosk9@gmail.com>
2020-01-04 13:52:01 -0600

    variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,7 @@
 
-var intervalId;
-var counter = 30;
+	var timer;
+	var timeLeft = 5;
 
-
-
-// function on click "start" from dispaly none to display the game 
-$("#resume").on("click", run);
-
-
-
-//timer 30 sec on start 
-
-function run() {
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-  }
-
-
-
- //  The decrement function.
- function decrement() {
-    number--;
-    $("#seconds").html("<h2>" + number + "</h2>");
-    if (number === 0) {
-    alert("well done loser"); 
-    }
-  }
+	var numCorrect = 0;
+	var numIncorrect = 0;
+	var numNull = 0;
